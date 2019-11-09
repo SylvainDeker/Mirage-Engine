4fdccaf05c12871b26938a829009828229682869
Sylvain DEKER <sldkr@gmx.com>
2019-11-09 20:10:17 +0100

    Add Texture loader

--- src/mesh/Texture.hpp
+++ src/mesh/Texture.hpp
@@ -3,11 +3,16 @@
 
 #include "glm/glm.hpp"
 #include <string>
+#include "../openGL/stb_image.h"
+#include "../openGL/opengl_stuff.h"
 
-struct Texture {
+class Texture {
+public:
     unsigned int id;
     std::string type;
     std::string path;
+
+    static unsigned int load(char const * path);
 };
 
 #endif // TEXTURE_H_
